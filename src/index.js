e6aeccf50582176b27252326f99eee23b004088c
molotoboy <molotoboy@gmail.com>
2019-09-21 12:43:59 +0600

    onClick set X

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  state={
+    value: null,
+  }
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={()=> this.setState({value:'X'})}>
+        {this.state.value}
       </button>
     );
   }
